f0bcb2c5e435ab5356f3adbe2613cea164740275
Bharat <bharatlakhera295@gmail.com>
2022-09-11 17:31:14 +0530

    9th Video Ended

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -3,24 +3,45 @@
 import React, {useState} from 'react'
 
 export default function TextForm(props) {
-    const [text, setText] = useState("Enter Your Text");
+    const [text, setText] = useState("");
     const handleUpClick = () => {
         // console.log(text);
         const myText = text.toUpperCase();
         setText(myText);
     }
+    const handleLoClick = () => {
+        // console.log(text);
+        const myText = text.toLowerCase();
+        setText(myText);
+    }
+    const handleClearClick = () => {
+        // console.log(text);
+        
+        setText("");
+    }
     const handleOnChange = (event) => {
         // console.log("Hello1");
         setText(event.target.value)
     }
-  return (
+    return (
+      <>
       <div>
-          <h1> {props.heading} - {text}</h1>
+          <h1> {props.heading}</h1>
           <div className="form-group">
-            <textarea className="form-control" id="exa" placeholder='Hello How are you ?' value={text} onChange = {handleOnChange} rows="5"></textarea>
-            <button className="btn btn-primary" onClick={handleUpClick}>Convert To UpperCase</button>
+            <textarea mb-3 className="form-control" id="exa" value={text} placeholder='Hello, How are you ?'  onChange = {handleOnChange} rows="5"></textarea>
          </div>
-      
-     </div>
+            <button className="btn btn-primary " onClick={handleUpClick}>Convert To UpperCase</button>
+            <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert To LowerCase</button>
+            <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear Text</button>
+         
+      </div>
+      <div className='my-3'>
+                <h1>Your Text's Info is Here !!</h1>
+                <p>It contains {text.length > 0 ?  text.split(" ").length : 0} words and {text.length} characters</p>      
+                <p>{(0.008 * (text.length > 0 ? text.split(" ").length : 0))} Minute read</p>
+                <h2>Preview</h2>
+                <p>{ text}</p>
+            </div>
+      </>
   )
 }
---
+++ src/components/t.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+void compute();
+int main(){
+#ifndef ONLINE_JUDGE
+  freopen("D://C++/input.txt", "r", stdin );
+  freopen("D://C++/output.txt","w", stdout);
+  freopen("D://C++/debug.txt", "w", stderr);
+#endif
+  int t;
+  cin >> t;
+  while(t--)
+  compute();
+}
+
+void compute(){
+     
+}
